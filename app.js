b998647f28c3fe9fc1e2be9a2319349cb97d645b
Jeremy Depenhart <jdevcreates@gmail.com>
2024-06-21 22:41:48 -0400

    Module 7 completed baseline

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//const hbs = require('hbs');
+const passport = require('passport');
+
+// Bring in database
+require('./app_api/models/db');
+
+require("./app_api/config/passport");
 
 // Define routers
 var indexRouter = require('./app_server/routes/index');
@@ -12,9 +21,6 @@ var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
 
-// Bring in database
-require('./app_api/models/db');
-
 var app = express();
 
 // view engine setup
@@ -22,6 +28,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
+//hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 
 app.set('view engine', 'hbs');
 
@@ -30,11 +37,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -48,6 +56,13 @@ app.use('/api', apiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+// catch 401 and forward to error handler
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
 });
 
 // error handler
--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-card',
@@ -10,14 +11,20 @@ import { Trip } from '../models/trip';
   templateUrl: './trip-card.component.html',
   styleUrl: './trip-card.component.css'
 })
+
 export class TripCardComponent implements OnInit{
 
   @Input('trip') trip: any;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
   }
 
   public editTrip(trip: Trip) {
